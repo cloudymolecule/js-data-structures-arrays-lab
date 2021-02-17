b58c941f382328860defef1b6fa7a92560c384a0
Cloudymolecule <baires82@gmail.com>
2021-02-16 20:46:36 -0500

    commit tree d7c84783c26fd9fe58afa5b81a2f4dc9506560b3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift()
+}
+
+function appendDriver(name){
+    const driversCopy = [...drivers, name]
+    return driversCopy
+}
+
+function prependDriver(name){
+    const driversCopy = [name, ...drivers]
+    return driversCopy
+}
+
+function removeLastDriver(){
+    const driversCopy = drivers.slice(0, drivers.length - 1)
+    return driversCopy
+}
+
+function removeFirstDriver(){
+    const driversCopy = drivers.slice(1)
+    return driversCopy
+}
